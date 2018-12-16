1a49268e02d8d7095fad019bd416b883c44f2410
Arun Kumar Devarajulu <arun89@terpmail.umd.edu>
2018-12-16 02:16:54 -0500

    Added header file for Vision class. The class will be used for taking snapshots of the turtlebot environment

---
+++ include/Vision.h
@@ -0,0 +1,65 @@
+/**************************************************************************************************
+* @file      : Vision.h
+* @author    : Arun Kumar Devarajulu
+* @date      : December 15, 2018
+* @copyright : 2018, Arun Kumar Devarajulu
+* @license   : MIT License
+*
+*              Permission is hereby granted, free of charge, to any person obtaining a copy
+*              of this software and associated documentation files (the "Software"), to deal
+*              in the Software without restriction, including without limitation the rights
+*              to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*              copies of the Software, and to permit persons to whom the Software is
+*              furnished to do so, subject to the following conditions:
+*
+*              The above copyright notice and this permission notice shall be included in all
+*              copies or substantial portions of the Software.
+*
+*              THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*              IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*              FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*              AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*              LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*              OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*              SOFTWARE.
+*
+* @brief     : The Vision.h class is used for accessing images taken by turtlebot's RGB camera
+****************************************************************************************************/
+#pragma once
+#ifndef VISION_H_
+#define VISION_H_
+
+#include "ros/ros.h"
+#include <iostream>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+class Vision {
+ public:
+	/**
+	*@brief : Default constructor
+	*/
+	Vision();
+
+	/**
+	*@brief : Default destructor
+	*/
+	~Vision();
+
+	/**
+	*@brief : Callback function for turtlebot camera subscriber
+	*/
+	void cameraCallback(const sensor_msgs::ImageConstPtr& picture);
+
+ private:
+ 	/**
+ 	*@brief : Create a nodehandle object
+ 	*/
+ 	ros::NodeHandle cn;
+};
+
+#endif
